329157338a325f8bf0321d1038268c2cbb4dc53b
Lpag96 <luigipago96@gmail.com>
2023-09-11 15:31:55 +0200

    Ho aggiunto come stai alla funzione main

--- Jaita92_11settembre2023_Marfiagithub/src/Main.java
+++ Jaita92_11settembre2023_Marfiagithub/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Buongiorno");
 		System.out.println("Buonasera");
-
+		System.out.println("come stai?");
 	}
 
 }
